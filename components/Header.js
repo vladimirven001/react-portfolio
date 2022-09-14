13de80e46cebf89756c6e3025ef93059f3cde7de
vlad <vladimir.venkov@mail.mcgill.ca>
2022-09-13 22:41:45 -0400

    header remove import nav dropdown

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,9 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Header() {
   return ( 
-  
-  
-    
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
@@ -19,7 +15,6 @@ export default function Header() {
           </Nav>
         </Container>
       </Navbar>
-      
     </>
   
   
