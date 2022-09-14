918914c9eea71aa3f5afc52b8a24cc7a46e1c165
vlad <vladimir.venkov@mail.mcgill.ca>
2022-09-13 21:38:42 -0400

    initial commit

---
+++ components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from '../styles/Footer.module.css'
+export default function p()
+{
+    return (
+        
+        <div className={styles.eqi_container}>
+            
+            <div>Vladimir Venkov</div> 
+            <div>(514) 963-4544 </div>  
+            <div>vladimir.venkov@mail.mcgill.ca</div>
+            
+        </div>
+
+    )
+
+}
