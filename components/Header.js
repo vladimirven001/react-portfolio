99d8eb6a65ac1273ae03350de6ac793430ce7ee0
vlad <vladimir.venkov@mail.mcgill.ca>
2022-09-13 21:52:56 -0400

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export default function Header() {
+  return ( 
+  
+  
+    
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+            <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#experience">Experience</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#education">Education</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      
+    </>
+  
+  
+
+)
+
+}
