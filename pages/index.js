aec759162cec7d9f8c926e8404dc2283100e737d
vlad <vladimir.venkov@mail.mcgill.ca>
2022-09-19 21:29:03 -0400

    homepage

--- pages/index.js
+++ pages/index.js
@@ -17,12 +17,20 @@ export default function Home() {
           <div className={styles.textSubtitle}>
             <p style={{fontFamily: "consolas"}}>software engineering student at mcgill university, montreal</p>
           </div>
-          <div className={styles.textSubtitle}>
-            <ol>
-              <li>experienced in salesmanship, leadership, and more</li>
-              <li>currently in the first year of a four year program of co-op software engineering</li>
-              <li>good knowledge in python, c#, html, css </li>
-            </ol>
+          <div className={styles.textList} style={{paddingTop: "15vh"}}>
+            <p style={{color: "#3DB8A5", fontFamily:"consolas"}}>01.{"\u00a0"}</p>
+            <p>experienced in salesmanship, leadership, and more{"\u00a0"}</p>
+            <a href="/experience" className={styles.textLink}>{"<"}learn more{">"}</a>
+          </div>
+          <div className={styles.textList}>
+            <p style={{color: "#3DB8A5", fontFamily:"consolas"}}>02.{"\u00a0"}</p>
+            <p>currently in the first year of a four year program of co-op software engineering{"\u00a0"}</p>
+            <a href="/education" className={styles.textLink}>{"<"}learn more{">"}</a>
+          </div>
+          <div className={styles.textList}>
+            <p style={{color: "#3DB8A5", fontFamily:"consolas"}}>03.{"\u00a0"}</p>
+            <p>good knowledge in python, c#, html, css and more{"\u00a0"}</p>
+            <a href="/skills" className={styles.textLink}>{"<"}learn more{">"}</a>
           </div>
 
           <Footer />
