5a415d6deed536051f0f75b2e9f853d335dbc510
vladimirven001 <vladimir.venkov@mail.mcgill.ca>
2022-09-30 16:27:58 -0400

    education pages

--- pages/education.js
+++ pages/education.js
@@ -1,15 +1,20 @@
 import React from "react";
 import Footer from '../components/Footer'
-
+import styles2 from '../styles/Home.module.css'
 const education = () => {
   return (
     <>
-      <div className="center">
-        <h2>education</h2>
+      <div className={styles2.container}>
+        <div className={styles2.principal}>
+          <div className="center">
+            <h2>education</h2>
+          </div>
+          <Footer />
+        </div>
       </div>
-      <Footer />
     </>
   );
 };
 
 export default education;
+
