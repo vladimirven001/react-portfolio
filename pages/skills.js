089a29f6d2e4443756b72a25970573594e2d6fb8
vladimirven001 <vladimir.venkov@mail.mcgill.ca>
2022-09-30 15:23:45 -0400

    skills done

--- pages/skills.js
+++ pages/skills.js
@@ -13,48 +13,48 @@ const skills = () => {
             <div className={styles.honey}>
               <div className={styles.row_1}>
                 <div className={styles.item}>
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" style={{height:"140px"}}/>
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
                 </div>
                 <div className={styles.item}>
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" style={{height:"140px"}}/>
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"  />
                 </div>
               </div>
 
               <div className={styles.row_2}>
                 <div className={styles.item}>
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"  />
                 </div>
                 <div className={styles.item}>
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/numpy/numpy-original.svg" style={{height:"140px"}}/>
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/numpy/numpy-original.svg"  />
                 </div>
                 <div className={styles.item}>
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"  />
                 </div>
                 <div className={styles.item}>
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"  />
                 </div>
                 <div className={styles.item}>
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"  />
                 </div>
               </div>
 
               <div className={styles.row_3}>
                 <div className={styles.item}>
-              
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"  />
                 </div>
                 <div className={styles.item}>
-              
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"  />
                 </div>
                 <div className={styles.item}>
-              
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/visualstudio/visualstudio-plain.svg"  />
                 </div>
                 <div className={styles.item}>
-              
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"  />
                 </div>
               </div>
               <div className={styles.row_4}>
                 <div className={styles.item}>
-              
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"  />
                 </div>
               </div>
             </div>
