156eebd884e168ed3da9cdae9df50643c380a216
vladimirven001 <vladimir.venkov@mail.mcgill.ca>
2023-10-02 17:24:28 -0400

    fixed education for small screends

--- pages/education.js
+++ pages/education.js
@@ -31,14 +31,14 @@ const education = () => {
             <div className={stylesEducation.bottomContainer}>
               <div className={stylesEducation.firstCard}>
                 <div className={stylesEducation.cardContent}>
-                    <p className={stylesEducation.preCardTextSubtitle}>09/2023</p>
+                    <p className={stylesEducation.preCardTextSubtitle}>10/2023</p>
                     <div className={stylesEducation.hoverContent}>
                       <p className={stylesEducation.cardTextSubtitle} style={{fontWeight:"bold"}}>09/2023</p>
-                      <div className={stylesMain.textList}>
+                      <div className={stylesEducation.textList}>
                         <p style={{color: "#dedede", fontFamily:"consolas", fontWeight:"bold"}}>01.{"\u00a0"}</p>
                         <p style={{color: "#dedede"}}> these are the credits i have accumulated as of now</p>
                       </div>
-                      <div className={stylesMain.textList}>
+                      <div className={stylesEducation.textList}>
                         <p style={{color: "#dedede", fontFamily:"consolas", fontWeight:"bold"}}>02.{"\u00a0"}</p>
                         <p style={{color: "#dedede"}}> i have acquired 37 credits out of the 113 required for my program</p>
                       </div>
@@ -51,11 +51,11 @@ const education = () => {
                     <p className={stylesEducation.preCardTextSubtitle}>05/2024</p>
                     <div className={stylesEducation.hoverContent}>
                       <p className={stylesEducation.cardTextSubtitle} style={{fontWeight:"bold"}}>05/2024</p>
-                      <div className={stylesMain.textList}>
+                      <div className={stylesEducation.textList}>
                         <p style={{color: "#dedede", fontFamily:"consolas", fontWeight:"bold"}}>01.{"\u00a0"}</p>
                         <p style={{color: "#dedede"}}> these are the credits i will have accumulated as of may 2024</p>
                       </div>
-                      <div className={stylesMain.textList}>
+                      <div className={stylesEducation.textList}>
                         <p style={{color: "#dedede", fontFamily:"consolas", fontWeight:"bold"}}>02.{"\u00a0"}</p>
                         <p style={{color: "#dedede"}}> i will have acquired 62 out of the 113 required credits</p>
                       </div>
