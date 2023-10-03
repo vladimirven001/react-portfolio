47792ae1039b1275e70aba299dfb25db3aa985a2
vladimirven001 <vladimirven001@gmail.com>
2023-10-02 22:06:32 -0400

    changed projects

--- pages/projects.js
+++ pages/projects.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import Footer from '../components/Footer'
 import stylesMain from '../styles/Home.module.css'
-import stylesExperience from '../styles/Contact.module.css';
-
+import stylesProjects from '../styles/Projects.module.css'
 
 const projects = () => {
   return (
     <>
       <div className={stylesMain.container}>
         <div className={stylesMain.principal}>
-          <div className={stylesExperience.textTitle}>
+          <div className={stylesProjects.textTitle}>
             <h1 style={{fontWeight:"normal"}}>projects</h1>
           </div>
-
           <Footer />
         </div>
       </div>
